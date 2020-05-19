a72dffe67940b033394ee22979e3dd696cd2dc2f
mark <markpackham1@gmail.com>
2020-05-19 15:01:43 +0100

    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <sightings-form />
+    <sightings-grid :sightings="sightings" />
+  </div>
+</template>
+
+<script>
+import SightingsForm from './components/SightingsForm';
+import SightingsGrid from './components/SightingsGrid';
+
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      sightings : {}
+    }
+  },
+  components: {
+    'sightings-form': SightingsForm,
+    'sightings-grid': SightingsGrid
+  },
+
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  background: url('./assets/birds-background.jpg') no-repeat;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+</style>
